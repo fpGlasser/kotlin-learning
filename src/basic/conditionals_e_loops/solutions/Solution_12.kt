f36b3d96c0832352e8e25972f3747b47d25301ce
FPGlasser <glasserpaulo95@hotmail.com>
2025-05-19 17:41:47 -0300

    solução da questão 12

--- src/basic/conditionals_e_loops/solutions/Solution_12.kt
+++ src/basic/conditionals_e_loops/solutions/Solution_12.kt
@@ -1,13 +1,14 @@
 package basic.conditionals_e_loops.solutions
 
 fun main(){
-    var contador = 0;
 
-    for(i: Int in 1 ..50){
+    var contador: Int = 0;
+
+    for(i: Int in 1..50){
         if(i % 2 == 0){
             contador++
         }
     }
 
-    println("Entre 1 e 50 existem $contador numeros pares");
+    println("Existem $contador numeros pares entre 1 e 50")
 }
