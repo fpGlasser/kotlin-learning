a05bbc80a5cc9c348c286d7ceffc3799af214da2
FPGlasser <glasserpaulo95@hotmail.com>
2025-05-19 04:01:15 -0300

    solução do exercicio 14

---
+++ src/basic/fundamentals/solutions/Solution_14.kt
@@ -0,0 +1,20 @@
+package basic.fundamentals.solutions
+
+fun main(){
+    val xByte: Byte = 2
+    val xInt: Int = 100
+    val xLong: Long = 27665553L
+    val xFloat: Float = 3.45F
+    val xDouble: Double = 52662.993877736
+    val xBoolean: Boolean = true
+    val xChar: Char = 'c'
+
+    println("Byte: $xByte")
+    println("Int: $xInt")
+    println("Long: $xLong")
+    println("Float: $xFloat")
+    println("Double: $xDouble")
+    println("Boolean: $xBoolean")
+    println("Char: $xChar")
+
+}
