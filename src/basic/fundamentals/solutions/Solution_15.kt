ac8578ec3a7a94021f5ca2ec6ab21cf992a6cd85
FPGlasser <glasserpaulo95@hotmail.com>
2025-05-19 04:06:40 -0300

    solução do exercicio 15

---
+++ src/basic/fundamentals/solutions/Solution_15.kt
@@ -0,0 +1,10 @@
+package basic.fundamentals.solutions
+
+fun main(){
+    val PI = 3.14159
+    val radius: Double = 5.0
+
+    val areaCircle = PI * Math.pow(radius, 2.0)
+
+    println("Area do circulo é: $areaCircle")
+}
