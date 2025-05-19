d8bbfb8bf6d992a29f53aade252f4db91bedb5d3
FPGlasser <glasserpaulo95@hotmail.com>
2025-05-19 03:04:01 -0300

    solução do exercicio 9

--- src/basic/fundamentals/solutions/Solution_09.kt
+++ src/basic/fundamentals/solutions/Solution_09.kt
@@ -1,2 +1,11 @@
 package basic.fundamentals.solutions
 
+fun main(){
+    print("Digite um numero: ");
+    val numero = readln().toInt()
+    if(numero % 2 == 0) {
+        println("O numero digitado é par")
+    }else{
+        println("O numero digitado é impar")
+    }
+}
