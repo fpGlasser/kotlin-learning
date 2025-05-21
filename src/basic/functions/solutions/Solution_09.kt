3cefe2f26c957a0769f806396991adec91d1ce4b
FPGlasser <glasserpaulo95@hotmail.com>
2025-05-21 12:58:04 -0300

    solução da questão 9

--- src/basic/functions/solutions/Solution_09.kt
+++ src/basic/functions/solutions/Solution_09.kt
@@ -1,2 +1,35 @@
 package basic.functions.solutions
 
+fun main(){
+    println("Digite uma frase: ")
+    val phrase: String =  readln()
+
+    val vogalQtd: Int = countVogal(phrase)
+    println("Esta frase contém $vogalQtd")
+
+}
+
+fun countVogal(phrase: String): Int {
+
+    var count: Int = 0
+
+    for (it in phrase.indices) {
+
+        if(phrase[it] == 'A' || phrase[it] == 'a'){
+            count++
+        }
+        else if(phrase[it] == 'E' || phrase[it] == 'e'){
+            count++
+        }else if(phrase[it] == 'I' || phrase[it] == 'i'){
+            count++
+        } else if(phrase[it] == 'O' || phrase[it] == 'o'){
+            count++
+        }else if(phrase[it] == 'U' || phrase[it] == 'u'){
+            count++
+        }else{
+            count += 0
+        }
+    }
+
+    return count
+}
