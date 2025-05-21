48ec0ec8247fe4f6c635fade58145b4d30954151
FPGlasser <glasserpaulo95@hotmail.com>
2025-05-21 01:50:13 -0300

    soluçÃão da questão 13

---
+++ src/basic/conditionals_e_loops/solutions/Solution_13.kt
@@ -0,0 +1,12 @@
+package basic.conditionals_e_loops.solutions
+
+fun main(){
+    print("Digite um numero para calcular fatorial: ")
+    val numero: Int = readln().toInt()
+    var fatorial: Int = 1
+    for(i: Int in numero downTo 1){
+        fatorial *= i;
+    }
+
+    println("Fatorial do numero $numero é: $fatorial")
+}
