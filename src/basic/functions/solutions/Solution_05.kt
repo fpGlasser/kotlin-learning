96bb5a91459a9f27d21d05cc350726068aaa9c0e
FPGlasser <glasserpaulo95@hotmail.com>
2025-05-21 11:32:43 -0300

    solução da questão 5

--- src/basic/functions/solutions/Solution_05.kt
+++ src/basic/functions/solutions/Solution_05.kt
@@ -1,2 +1,16 @@
 package basic.functions.solutions
 
+import java.text.DecimalFormat
+import kotlin.math.ceil
+
+fun main(){
+    print("Digite um numero: ")
+    val number: Double = readln().toDouble()
+
+    val result: Double = ceilRound(number)
+    println("Numero $number arredondado para cima: $result")
+}
+
+fun ceilRound(num: Double): Double{
+    return DecimalFormat("##.##").format(ceil(num)).toDouble()
+}
