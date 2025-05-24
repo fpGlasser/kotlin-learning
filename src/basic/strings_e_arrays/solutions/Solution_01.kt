f86162f00297b16b05dba76db859753cafb0638f
FPGlasser <glasserpaulo95@hotmail.com>
2025-05-23 22:28:31 -0300

    solução da questão 1

---
+++ src/basic/strings_e_arrays/solutions/Solution_01.kt
@@ -0,0 +1,8 @@
+package basic.strings_e_arrays.solutions
+
+fun main(){
+    println("Digite ume frase: ");
+    val phrase: String = readln()
+
+    println("A frase digitada tem ${phrase.length} caracteres");
+}
